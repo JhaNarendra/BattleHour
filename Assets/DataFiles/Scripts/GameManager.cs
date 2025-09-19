8f24aee22bb6122e18a1719d7b362dc7f0c0f5e7
Narendra Jha <56018997+JhaNarendra@users.noreply.github.com>
2025-09-19 11:41:18 +0530

    Commit#10

--- Assets/DataFiles/Scripts/GameManager.cs
+++ Assets/DataFiles/Scripts/GameManager.cs
@@ -1,16 +1,28 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public int loadGame;
     private void Awake()
     {
-         AsyncOperationHandle<GameObject> asyncOperationHandle = Addressables.LoadAssetAsync<GameObject>("Assets/Model/Prefabs/City.prefab");
+        AsyncOperationHandle<GameObject> asyncOperationHandle = Addressables.LoadAssetAsync<GameObject>("Assets/Model/Prefabs/City.prefab");
 
-         asyncOperationHandle.Completed += AsyncOperationHandle_completed;
+        asyncOperationHandle.Completed += AsyncOperationHandle_completed;
     }
 
+    private async void Start()
+    {
+        await Task.Delay(5000);
+        if (loadGame != 0)
+        {
+            SceneManager.LoadScene(loadGame);
+            return;
+        }
+    }
     private void AsyncOperationHandle_completed(AsyncOperationHandle<GameObject> asyncOperationHandle)
     {
         if(asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
